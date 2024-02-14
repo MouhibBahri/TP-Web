518dc956ba58e2bcc04bd7b438ccc19762de43cd
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2024-02-14 15:40:26 +0100

    added ex2

---
+++ TP1/mini-projet-2/script.js
@@ -0,0 +1,16 @@
+const paragraph=document.querySelector("#content");
+const fontSelector=document.querySelector("#fontSelector");
+const colorSelector=document.querySelector("#colorSelector");
+const numberSelector=document.querySelector("#numberSelector");
+
+paragraph.style.color=colorSelector.value;
+
+fontSelector.addEventListener("change",()=>{
+    paragraph.style.fontFamily=fontSelector.value;
+});
+colorSelector.addEventListener("change",()=>{
+    paragraph.style.color=colorSelector.value;
+});
+numberSelector.addEventListener("change",()=>{
+    paragraph.style.fontSize=numberSelector.value+"px";
+})
