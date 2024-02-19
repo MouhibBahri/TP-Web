657b495e77a5a02007ce6f1c1c0e8bbe3cf3a185
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2024-02-19 23:28:31 +0100

    fix

--- ex1/script.js
+++ ex1/script.js
@@ -1,11 +1,51 @@
-function easy() {
-    window.location.href = "../ex1/first/first.html";
-}
+document.querySelector('#easy').addEventListener('click',()=> play(0));
+document.querySelector('#inter').addEventListener('click',()=> play(1));
+document.querySelector('#hard').addEventListener('click',()=> play(2));
 
-function inter() {
-    window.location.href = "../ex1/second/second.html";
-}
+const nbAttempts=5;
+const max=[10,30,100];
+const msg=["Easy","Intermediate","Hard"];
+function play(level) {
+    const random = Math.floor(Math.random() * max[level]+1) ;
+    let i=0;
 
-function hard() {
-    window.location.href = "../ex1/third/third.html";
+    while (i < nbAttempts) {
+        let number = prompt(msg[level]+" number: give me a number between 0 and "+max[level]);
+        while(number==="")
+            number = prompt(msg[level]+" number: give me a number between 0 and "+max[level]);
+
+        number = Number(number);
+        if (number > max[level] || number <0) {
+            alert("Invalid, try again");
+            continue;
+        }
+        if (number === random) {
+            alert("You WON!!");
+            break;
+        } else {
+            let message;
+            switch (level){
+                case 0:
+                    message = number < random ? "Try a higher number" : "Try a lower number";
+                    break;
+                case 1:
+                    const x=Math.abs(number-random);
+                    message="The difference is between 1 and "+x;
+                    break;
+                case 2:
+                    message="Try another number";
+                    break;
+            }
+
+            alert("Oups!! "+message);
+            i++;
+        }
+    }
+    if (i === nbAttempts) {
+        alert("You LOST!!");
+    }
+    const tryAgain = confirm("Do you want to play again ?");
+    if (tryAgain) {
+        play(level);
+    }
 }
--- ex3/script.js
+++ ex3/script.js
@@ -1,11 +1,20 @@
-document.querySelector('button').addEventListener("click", ()=>{
-    const name=document.getElementById("name");
-    const content=document.getElementById("content");
-    document.querySelector('.left').innerHTML+=`<span> ${name.value.toString()} : ${content.value.toString()}🗑</span>`;
+const name=document.getElementById("name");
+const content=document.getElementById("content");
+const button=document.querySelector('button');
+const inputs=document.querySelectorAll('input');
+inputs.forEach((input)=>{
+    input.addEventListener('input',()=>{
+        button.disabled = inputs[0].value.trim() === "" || inputs[1].value.trim() === "";
+
+    })
+});
+button.addEventListener("click", ()=>{
+    document.querySelector('#list').innerHTML+=`<span> ${name.value.trim()} : ${content.value.trim()} 🗑</span>`;
     name.value=content.value="";
+    button.disabled=true;
 });
 
-document.querySelector(".left").addEventListener("click", (event)=>{
+document.querySelector("#list").addEventListener("click", (event)=>{
 
     if (event.target.tagName==='SPAN'){
         event.target.parentNode.removeChild(event.target);
